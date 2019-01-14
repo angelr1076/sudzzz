f48bb71e7f9e6d9b1da0c3ca8027284f52eb6060
Angel <angelr1076@gmail.com>
2019-01-14 14:32:21 -0500

    removed semicolons

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
-  const addItems = document.querySelector('.add-items');
-  const removeItems = document.querySelector('.remove-items');
-  const buttons = document.querySelectorAll('.button');
-  const itemsList = document.querySelector('.beers');
+  const addItems = document.querySelector('.add-items')
+  const removeItems = document.querySelector('.remove-items')
+  const buttons = document.querySelectorAll('.button')
+  const itemsList = document.querySelector('.beers')
   // if there is no item string, create an empty array
-  const items = JSON.parse(localStorage.getItem('items')) || [];
+  const items = JSON.parse(localStorage.getItem('items')) || []
 
   function addItem(e) {
     // prevent the form from submitting
-    e.preventDefault();
-    const text = (this.querySelector('[name=item]')).value;
+    e.preventDefault()
+    const text = (this.querySelector('[name=item]')).value
     const item = {
       text,
       done: false
     }
-    items.push(item);
+    items.push(item)
     // rerenders list without having to reload the page
-    populateList(items, itemsList);
-    localStorage.setItem('items', JSON.stringify(items));
-    this.reset();
+    populateList(items, itemsList)
+    localStorage.setItem('items', JSON.stringify(items))
+    this.reset()
   }
 
   // function removes all items in the array
   const removeAllItems = e => {
-    e.preventDefault();
-    items.splice(0);
-    localStorage.setItem('items', JSON.stringify(items));
-    populateList([], itemsList);
+    e.preventDefault()
+    items.splice(0)
+    localStorage.setItem('items', JSON.stringify(items))
+    populateList([], itemsList)
   }
 
   // toggle a single item
   const toggleDone = e => {
-    if (!e.target.matches('input')) return; // skip this unless it's an input
-    const el = e.target;
-    const index = el.dataset.index;
+    if (!e.target.matches('input')) return // skip this unless it's an input
+    const el = e.target
+    const index = el.dataset.index
     // toggle from done to !done
-    items[index].done = !items[index].done;
-    localStorage.setItem('items', JSON.stringify(items));
+    items[index].done = !items[index].done
+    localStorage.setItem('items', JSON.stringify(items))
   }
 
   // toggle inventory of all items to true or false
   const toggleAllItems = e => {
     items.forEach((item, index, array) => {
-      e.target.name === 'checkAll' ? (items[index].done = true) : (items[index].done = false);
+      e.target.name === 'checkAll' ? (items[index].done = true) : (items[index].done = false)
     })
-    localStorage.setItem('items', JSON.stringify(items));
-    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items))
+    populateList(items, itemsList)
   }
 
   const populateList = (beers = [], beerList) => {
@@ -55,14 +55,14 @@
           <input type="checkbox" data-index=${i} id="item${i}" ${beer.done ? 'checked' : ''} />
           <label for="item${i}">${beer.text}</label>
         </li>
-      `;
-    }).join('');
+      `
+    }).join('')
   }
 
-  addItems.addEventListener('submit', addItem);
-  removeItems.addEventListener('click', removeAllItems);
-  itemsList.addEventListener('click', toggleDone);
-  buttons.forEach((button) => button.addEventListener('click', toggleAllItems));
+  addItems.addEventListener('submit', addItem)
+  removeItems.addEventListener('click', removeAllItems)
+  itemsList.addEventListener('click', toggleDone)
+  buttons.forEach((button) => button.addEventListener('click', toggleAllItems))
   
   // call populate to render the list on page load
-  populateList(items, itemsList);
+  populateList(items, itemsList)
